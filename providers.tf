fce8b8316bee5b2245d0366ce555b4c3825daa20
Roger Chen <mrrochen@gmail.com>
2024-03-24 09:22:23 -0400

    1provider n rg

---
+++ providers.tf
@@ -0,0 +1,24 @@
+# Initialises Terraform providers and sets their version numbers.
+
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.97.1"
+    }
+  }
+}
+
+# Documentation: https://www.terraform.io/docs/language/values/variables.html
+variable "azure_subscription_id" {
+  type = string
+}
+
+# Documentation: https://www.terraform.io/docs/language/providers/requirements.html
+# Documentation: https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs
+provider "azurerm" {
+  features {}
+
+  subscription_id = var.azure_subscription_id
+}
